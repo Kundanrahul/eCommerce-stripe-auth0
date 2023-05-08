2b7c91dee905ca3229bd8946e32ca81df84360da
Rahul Kundan <112650144+Kundanrahul@users.noreply.github.com>
2023-05-09 00:22:14 +0530

    Update MotionWrap.jsx

--- MotionWrap/MotionWrap.jsx
+++ MotionWrap/MotionWrap.jsx
@@ -5,7 +5,7 @@ const MotionWrap = ({ children }) => {
   return (
     <motion.div
       whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-      transition={{ duration: 0.4, ease: "easeOut" }}
+      transition={{ duration: 0.7, ease: "easeOut" }}
     >
       {children}
     </motion.div>
